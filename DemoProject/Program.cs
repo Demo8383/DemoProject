71b7871576eb1d56b6eee0a067539fb2cb689047
whomeandyou <munhaw.kok@appfuxion.com>
2021-11-10 16:00:22 +0800

    111

--- DemoProject/Program.cs
+++ DemoProject/Program.cs
@@ -11,7 +11,10 @@ namespace DemoProject
             Console.WriteLine("New hello");
             Console.WriteLine("New hello");
             Console.WriteLine("New hello");
-            Console.WriteLine("New hello"); Console.WriteLine("New hello"); ; ; 
+            Console.WriteLine("New hello"); 
+            Console.WriteLine("New hello");
+            Console.WriteLine("New hello");
+            Console.WriteLine("New hello");
         }
     }
 }
